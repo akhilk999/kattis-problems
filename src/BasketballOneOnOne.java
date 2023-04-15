cf00355531b4cde6f53e8c6643e30170ddb1853d
Akhil Kasamsetty <awesomesetty99@gmail.com>
2023-04-15 00:19:05 -0500

    added BasketballOneOnOne.java & Zoom.java

---
+++ src/BasketballOneOnOne.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+//import java.util.Scanner;
+
+public class BasketballOneOnOne{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        System.out.print(s.charAt(s.length()-2));
+    }
+}
---
+++ src/Zoom.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+//import java.util.Scanner;
+//import java.util.Arrays;
+
+public class Zoom{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        scan.nextLine();
+        int q = n/k;
+        int [] line = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            line[i] = scan.nextInt();
+        }
+        for(int i = k-1; i<n;i=i+k)
+        {
+            System.out.print(line[i]+" ");
+        }
+    }
+}
